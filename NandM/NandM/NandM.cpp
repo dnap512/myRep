b0a7118e29e1b216868cb214968591828828ab6d
김대희 <frshield2@kookmin.kr>
2019-03-13 19:19:47 +0900

    N과M 재귀

---
+++ NandM/NandM/NandM.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+static bool c[10];
+static int list[10];
+
+void NM(int index, int n, int m) {
+	if (index == m+1) {
+		for (int i = 1; i <= m; i++) {
+			cout << list[i] << " ";
+		} cout << endl;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (c[i]) continue;
+		c[i] = true;
+		list[index] = i;
+		NM(index + 1, n, m);
+		c[i] = false;
+	}
+}
+
+int main(){
+	int n, m;
+	cin >> n >> m;
+
+	NM(1, n, m);
+}
