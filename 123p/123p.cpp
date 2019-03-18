ebf6778b57ffa23974a4befbf169a18a086ec2bd
김대희 <frshield2@kookmin.kr>
2019-03-18 23:31:31 +0900

    baekjoon

---
+++ 123p/123p.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+static int memo[12];
+
+int p123(int n) {
+	if (memo[n] > 0) {
+		return memo[n];
+	}
+	memo[n] = p123(n - 3) + p123(n - 2) + p123(n - 1);
+	return memo[n];
+}
+
+int main()
+{
+	int test;
+	int n;
+	cin >> test;
+	memo[0] = 1;
+	memo[1] = 1;
+	memo[2] = 2;
+	while (test--) {
+		cin >> n;
+
+		cout << p123(n) <<  endl;
+	}
+}
---
+++ 2x1tile/2x1tile/2x1tile.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "pch.h"
+using namespace std;
+
+static int memo[1001];
+
+int tile(int n) {
+	if (memo[n] > 0) {
+		return memo[n] % 10007;
+	}
+	memo[n] = tile(n - 1) % 10007 + tile(n - 2) % 10007;
+	return memo[n] % 10007;
+	
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	memo[0] = 1;
+	memo[1] = 1;
+	memo[2] = 2;
+	cout << tile(n) << endl;
+	return 0;
+}
